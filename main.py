6e66c296b32457aa477fc62ba82b9558aa6a3f3f
lucas <lucasmacielaguiar@gmail.com>
2019-04-05 00:33:31 -0300

    change local of search methods

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-def selection_sort(vector = []):
-    index_min = 0
-    aux = 0
-    for i in range(len(vector) - 1):
-        index_min = i
-        for j in range(i, len(vector) - 1):
-            if vector[j] < vector[index_min]:
-                index_min = j
-        if index_min != i:
-            aux = vector[index_min]
-            vector[index_min] = vector[i]
-            vector[i] = aux
-    return vector
-
-def insertion_sort(vector = []):
-    aux = 0
-    for i in range(1, len(vector) - 1):
-        j = i
-        while(vector[j] < vector[j - 1] and j > 0):
-            aux = vector[j]
-            vector[j] = vector[j - 1]
-            vector[j - 1] = aux
-            j-=1
-    return vector
-
-def bubble_sort(vector = []):
-    aux = 0
-    while True:
-        count = 0
-        for i in range(1, len(vector) - 1):
-            if vector[i] < vector[i-1]:
-                aux = vector[i]
-                vector[i] = vector[i-1]
-                vector[i-1] = aux
-                count = 1
-        if count == 0:
-            return vector
-        
-    
+from searchMethods import *
+from listArcives import *
+from movie import *
 
 print(selection_sort([3, 5, 1, 9]))
 print(insertion_sort([8, 1, 3, 2, 10]))
--- movie.py
+++ movie.py
@@ -3,6 +3,6 @@ import pygame
 
 pygame.display.set_caption('My video!')
 
-clip = VideoFileClip('videos/CLEAN CODE 1 Introdução.mp4')
+clip = VideoFileClip('videos/cut.mp4')
 clip.preview()
 pygame.event.wait()
